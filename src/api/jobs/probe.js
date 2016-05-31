c1619fda9faa0617d0f035e8f7b8e8d2f9e86521
Jordan Van Walleghem <j.vanwall@gmail.com>
2016-05-31 20:31:01 +0200

    Twitter !

--- src/api/jobs/probe.js
+++ src/api/jobs/probe.js
@@ -1,12 +1,26 @@
 'use strict';
 
 const Promise = require('bluebird');
+const Twitter = require('twitter');
+const numeral = require('../../app/src/services/numeral');
 
 const orm = require('../orm');
 const extractors = require('../extractors');
 const postmark = require("postmark")(process.env.POSTMARK_API_TOKEN);
+const twitterClient = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 const sendMail = Promise.promisify(postmark.send, { context: postmark });
+const tweet = (status) => new Promise((resolve, reject) => {
+  twitterClient.post('statuses/update', { status }, (err, tweet) => {
+    if (err) return reject(err);
+    return resolve(tweet);
+  });
+});
 
 const createProbeAndNotify = (resellerVideoCard, latestProbe, { price, inStock }) =>
     orm.models.Probe.create({
@@ -15,15 +29,23 @@ const createProbeAndNotify = (resellerVideoCard, latestProbe, { price, inStock }
       ResellerVideoCardId: resellerVideoCard.id
     })
       .then(probe => {
-        if (probe.inStock === true && latestProbe.inStock === false) {
-          return resellerVideoCard.getVideoCard()
-            .then(videoCard => videoCard.getUsers())
+        if (!latestProbe) {
+          tweet(`Les ${ resellerVideoCard.VideoCard.Manufacturer.name } ${ resellerVideoCard.VideoCard.name } sont maintenant listés chez ${ resellerVideoCard.Resseller.name } ! ${ resellerVideoCard.url }`);
+        }
+        if (latestProbe && latestProbe.price > probe.price) {
+          tweet(`Baisse de prix sur les ${ resellerVideoCard.VideoCard.Manufacturer.name } ${ resellerVideoCard.VideoCard.name } chez ${ resellerVideoCard.Resseller.name } ancien prix: ${ numeral(latestProbe.price).format('0,0[.]00 $') }, nouveau prix: ${ numeral(probe.price).format('0,0[.]00 $') }! ${ resellerVideoCard.url }`);
+        }
+        if (probe.inStock === true && (!latestProbe || latestProbe.inStock === false)) {
+          return tweet(`Les ${ resellerVideoCard.VideoCard.Manufacturer.name } ${ resellerVideoCard.VideoCard.name } viennent de passer en stock chez ${ resellerVideoCard.Resseller.name } ! ${ resellerVideoCard.url }`)
+            .catch(e => console.error(e, 'tweet error'))
+            .then(() => resellerVideoCard.VideoCard.getUsers())
             .each(user => sendMail({
               "From": "jordan@telephone-ro.se",
               "To": user.email,
-              "Subject": "Une GTX 1080 est en stock !",
+              "Subject": `${ resellerVideoCard.VideoCard.Manufacturer.name } ${ resellerVideoCard.VideoCard.name } en stock chez ${ resellerVideoCard.Resseller.name } !`,
               "TextBody": `
-                YO, on dirait bien qu'une 1080 vient de passer en stock :o
+                Yo, on dirait bien que les ${ resellerVideoCard.VideoCard.Manufacturer.name } ${ resellerVideoCard.VideoCard.name } viennent de passer en stock chez ${ resellerVideoCard.Resseller.name } !
+
                 ${ resellerVideoCard.url }
 
                 Se desabo https://gfx-track.herokuapp.com/unregister?email=${ user.email } :(
@@ -39,7 +61,10 @@ const createProbeAndNotify = (resellerVideoCard, latestProbe, { price, inStock }
 module.exports = () =>
   Promise.try(() => orm.models.ResellerVideoCard.findAll({
     include: [
-      { model: orm.models.Reseller }
+      { model: orm.models.Reseller},
+      { model: orm.models.VideoCard, include: [
+        { model: orm.models.Manufacturer }
+      ] }
     ]
   }))
   .each(resellerVideoCard => {
