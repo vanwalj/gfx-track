a3eec48077b87a2af07719cf875c384964f710df
Jordan Van Walleghem <j.vanwall@gmail.com>
2016-06-03 16:46:02 +0200

    Update index.js

--- src/api/extractors/index.js
+++ src/api/extractors/index.js
@@ -5,6 +5,7 @@ const AmazonFrExtractor = require('./amazon-fr');
 const MaterielPointNetExtractor = require('./materiel-point-net');
 const TopachatExtractor = require('./topachat');
 const GrosbillExtractor = require('./grosbill');
+const NvidiaFranceExtractor = require('./nvidia-fr');
 
 // Slug to class
 module.exports = {
@@ -12,5 +13,6 @@ module.exports = {
   materielPointNet: MaterielPointNetExtractor,
   topachat: TopachatExtractor,
   amazonFr: AmazonFrExtractor,
-  grosbill: GrosbillExtractor
+  grosbill: GrosbillExtractor,
+  nvidiaFr: NvidiaFranceExtractor
 };
